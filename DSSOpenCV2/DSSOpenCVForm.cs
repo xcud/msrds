5fe9b4ac8538c66c622454de40198179ec5107e7
perpet99 <perpet99@cc61708c-8d4c-0410-8fce-b5b73d66a671>
2008-06-22 19:23:27 +0000

    aaa

---
+++ DSSOpenCV2/DSSOpenCVForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robotics.Dssopencv2
+{
+    public class DSSOpenCVForm : CLROpenCV.MovingDetect
+    {
+
+        Dssopencv2Operations _eventsPort;
+
+        public DSSOpenCVForm(Dssopencv2Operations eventsPort)
+        {
+            _eventsPort = eventsPort;
+
+        }
+        
+    }
+}
